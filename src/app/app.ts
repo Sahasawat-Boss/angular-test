642a38b05bf3767784285a8504a332f8b81bc1c7
Sahasawat-Boss <sahasawat.r@softdebut.com>
2025-07-23 17:25:56 +0700

    remove latest comps

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,9 +7,16 @@ import { Footer } from './components/footer/footer';
 import { Hero } from './components/hero/hero';
 import { AboutComponent } from './components/about/about';
 
+
+//Material Imports
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // 👈 สำคัญมากสำหรับ Angular Material
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatCardModule} from '@angular/material/card';
+
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Footer, Hero, AboutComponent],
+  imports: [RouterOutlet, Navbar, Footer, Hero, AboutComponent, MatSlideToggleModule, MatCardModule ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
