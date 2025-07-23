3dcece30263ea89b6a4a0fd9160291a97d49bc7e
Sahasawat-Boss <sahasawat.r@softdebut.com>
2025-07-23 16:09:30 +0700

    update hero nav foot comps

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './navbar/navbar';
+import { Footer } from './footer/footer';
+import { Hero } from './hero/hero';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Footer, Hero],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
